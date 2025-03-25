6d83b7f240560d78427f68a792c8b38e809fcd70
Hripsime Melikyan <hripsime.melikyan@yahoo.com>
2025-03-25 21:58:47 +0400

    first commit

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,30 @@
+const initialGameBoard = [
+    [null,null,null],
+    [null,null,null],
+    [null,null,null],
+];
+
+export default function GameBoard({onSelectSquare, turns}){
+    let gameBoard = initialGameBoard.map(row => [...row]);
+
+    for(const turn of turns){
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
+        console.log(gameBoard);
+    }
+
+    return( <ol id = "game-board">
+        {gameBoard.map((row, rowIndex)=> ( <li key={rowIndex}>
+        <ol>
+            {row.map((playerSymbol, colIndex)=> (
+            <li key={colIndex}>
+                <button onClick = {() =>onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+            </li>
+        ))}
+        </ol>
+        </li>
+    ))}
+    </ol>);
+}
