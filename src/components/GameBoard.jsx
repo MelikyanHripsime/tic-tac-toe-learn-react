d806e5a2fd005044c2d30f204e0ed2886a91cf46
Hripsime Melikyan <hripsime.melikyan@yahoo.com>
2025-05-02 15:34:03 +0400

    tic tac toe working game

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,26 +1,12 @@
-const initialGameBoard = [
-    [null,null,null],
-    [null,null,null],
-    [null,null,null],
-];
-
-export default function GameBoard({onSelectSquare, turns}){
-    let gameBoard = initialGameBoard.map(row => [...row]);
-
-    for(const turn of turns){
-        const {square, player} = turn;
-        const {row, col} = square;
-
-        gameBoard[row][col] = player;
-        console.log(gameBoard);
-    }
-
+export default function GameBoard({onSelectSquare, board}){
     return( <ol id = "game-board">
-        {gameBoard.map((row, rowIndex)=> ( <li key={rowIndex}>
+        {board.map((row, rowIndex)=> ( <li key={rowIndex}>
         <ol>
             {row.map((playerSymbol, colIndex)=> (
             <li key={colIndex}>
-                <button onClick = {() =>onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                <button onClick = {() =>onSelectSquare(rowIndex,colIndex)} disabled = {playerSymbol !== null}>
+                    {playerSymbol}
+                </button>
             </li>
         ))}
         </ol>
